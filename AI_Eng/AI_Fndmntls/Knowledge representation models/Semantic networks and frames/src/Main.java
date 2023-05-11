e28834454d6c2f55aa10c393a58b96ee21d6f4f2
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-05-11 12:54:53 -0600

    wtf with prolog

---
+++ AI_Eng/AI_Fndmntls/Knowledge representation models/Semantic networks and frames/src/Main.java
@@ -0,0 +1,10 @@
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        AnimalBook e = new AnimalBook();
+        e.setVisible(true);
+
+    }
+}
