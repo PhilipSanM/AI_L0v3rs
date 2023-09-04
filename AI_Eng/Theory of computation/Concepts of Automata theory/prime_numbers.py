fe8eceb781cbb50a50ee4c8bc4ac347636a36a26
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-09-03 20:08:24 -0600

    prime_numbers

--- AI_Eng/Theory of computation/Concepts of Automata theory/prime_numbers.py
+++ AI_Eng/Theory of computation/Concepts of Automata theory/prime_numbers.py
@@ -4,11 +4,45 @@ from collections import deque
 n = int(input("Pls give me the max number to check prime numbers or just press 'enter': "))
 
 
+
 # Output file
 sys.stdout = open('outputf.txt','w')
 
+
+if n == '':
+    exit()
+    n = random.randint(0, 10000)
+
+print("{", end = '')
+
 # Code goes here
 # ========================================
+from math import sqrt
+
+def isPrime(n):
+    if(n > 1):
+        for i in range(2, int(sqrt(n)) + 1):
+            if (n % i == 0):
+                return False
+
+        return True
+    else:
+        return False
+    
+prime_numbers = []
+
+for number in range(n + 1):
+    if (isPrime(number)):
+        prime_numbers.append(number)
+        print(bin(number)[2::], end = ', ')
+
+print('}')
+
+
+# ========================================
+# Closing files
+sys.stdout.close()
+
 
 # GRAPH
 import pandas as pd
@@ -31,17 +65,37 @@ chain        number_of_1s
 
 '''
 
-chain = [1]   # empty = epsilon
-number_of_1s = [0] # 0 
+chain = []   # empty = epsilon
+number_of_1s = [] # empty = epsilon
 
-for i in range(2, bits + 2):
+
+def hammingWeight(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    mask = 1
+    count = 0
+    while n != 0:
+        if (mask & n == 1):
+            count = count + 1
+        n = n>>1
+
+    return count
+
+i = 1
+
+for prime in prime_numbers:
     chain.append(i)
-    number_of_1s.append(bin(i - 2).count('1'))
+    i += 1
+    number_of_1s.append(hammingWeight(prime))
+
+    
 
 df = pd.DataFrame({'chain': chain, 'number_of_1s': number_of_1s})
 
 # save to csv file
-df.to_csv('Binary_Strings.csv', index = False)
+df.to_csv('primes.csv', index = False)
 
 
 # Plotting
@@ -55,6 +109,3 @@ plt.show()
 
 
 
-# ========================================
-# Closing files
-sys.stdout.close()
