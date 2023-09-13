c6883e4b4263080e0478a2679de6a52694a7bd8d
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-09-12 18:11:54 -0600

    Functions

--- AI_Eng/bio_inspired algorithms/Intro2bioinspiredalgorithms/functions.py
+++ AI_Eng/bio_inspired algorithms/Intro2bioinspiredalgorithms/functions.py
@@ -54,9 +54,36 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
   cmap=cm.nipy_spectral, linewidth=0.08,
   antialiased=True)
-# plt.savefig('rastrigin_graph.png')
+plt.savefig('rastrigin_graph.png')
 plt.show()
 
 
 
-# Ackley function
+# Ackley.py
+from numpy import arange
+from numpy import exp
+from numpy import sqrt
+from numpy import cos
+from numpy import e
+from numpy import pi
+from numpy import meshgrid
+import matplotlib.pyplot as plt
+
+def objective(x, y):
+ return -20.0 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2)))-exp(0.5 * (cos(2 * 
+  pi * x)+cos(2 * pi * y))) + e + 20
+
+
+r_min, r_max = -32.768, 32.768
+xaxis = arange(r_min, r_max, 2.0)
+yaxis = arange(r_min, r_max, 2.0)
+x, y = meshgrid(xaxis, yaxis)
+results = objective(x, y)
+
+figure = plt.figure()
+axis = figure.add_subplot(111, projection='3d')
+axis.plot_surface(x, y, results, cmap='jet', shade= "false")
+plt.show()
+
+
+
